2decbaeec656538305bed9f8af4586dfe4ca5762
m3rashid <m3rashid.hussain@gmail.com>
2024-05-05 16:39:31 +0530

    udpate 7

--- components/map.js
+++ components/map.js
@@ -205,14 +205,15 @@ const styles = StyleSheet.create({
 const pickerSelectStyles = StyleSheet.create({
 	inputIOS: {
 		fontSize: 16,
-		paddingVertical: 12,
+		// paddingVertical: 12,
+		paddingTop: 12,
 		paddingHorizontal: 10,
-		borderWidth: 1,
-		borderColor: 'gray',
+		// borderWidth: 1,
+		// borderColor: 'gray',
 		borderRadius: 4,
 		color: 'black',
-		width: 160,
-		marginLeft: 50,
+		width: 200,
+		marginLeft: 20,
 		paddingRight: 30, // to ensure the text is never behind the icon
 	},
 	inputAndroid: {
